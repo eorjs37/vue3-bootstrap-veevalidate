f2aa861a7baccbfe4022abecc12237da7f2300ff
CHOI DAE GEON <chleorjs37@gmail.com>
2022-08-08 21:46:37 +0900

    toast ui 확인

---
+++ src/pages/editor.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>chart</h1>
+  <div id="chart"></div>
+</template>
+
+<script>
+import { LineChart } from '@toast-ui/chart';
+import { onMounted } from 'vue';
+export default {
+  components: {},
+  setup() {
+    onMounted(() => {
+      const el = document.getElementById('chart');
+      const data = {
+        categories: ['표1', '표2', '표3'],
+        series: [
+          { name: '1인', data: [1, 2, 3] },
+          { name: '2인', data: [4, 5, 6] },
+          { name: '3인', data: [7, 8, 9] },
+        ],
+      };
+      const options = {
+        chart: { width: 700, height: 400 },
+      };
+      new LineChart({ el, data, options });
+    });
+
+    return {};
+  },
+};
+</script>
+
+<style></style>
