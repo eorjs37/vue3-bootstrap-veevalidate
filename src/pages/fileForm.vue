c5edfca1cdb51e33a525ab65fcb0969928ab0757
CHOI DAE GEON <chleorjs37@gmail.com>
2022-08-15 11:48:54 +0900

    파일 체크 custom

---
+++ src/pages/fileForm.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="container mt-3">
+    <Form @submit="fileForm.onsubmit" ref="addFileForm">
+      <b-row>
+        <b-col lg="6" sm="12">
+          <Field v-model="fileForm.name" id="name" name="name" label="이름" rules="required" v-slot="{ field, errors }">
+            <b-form-group id="name" label="이름" label-for="name">
+              <b-form-input id="name" v-bind="field" type="text" placeholder="이름을 입력하세요." :state="errors[0] ? false : null"> </b-form-input>
+
+              <b-form-invalid-feedback v-if="errors[0]" id="input-live-feedback"> {{ errors[0] }} </b-form-invalid-feedback>
+            </b-form-group>
+          </Field>
+        </b-col>
+        <b-col lg="6" sm="12">
+          <Field v-model="fileForm.email" id="email" name="email" label="이메일" rules="required|email" v-slot="{ field, errors }">
+            <b-form-group id="email" label="Email address" label-for="email">
+              <b-form-input
+                id="email"
+                v-bind="field"
+                type="email"
+                placeholder="Enter email"
+                :state="errors[0] ? false : null"
+                required></b-form-input>
+
+              <b-form-invalid-feedback v-if="errors[0]" id="input-live-feedback"> {{ errors[0] }} </b-form-invalid-feedback>
+            </b-form-group>
+          </Field>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col lg="12" sm="12">
+          <Field v-model="fileForm.file" id="file" name="file" label="첨부파일" rules="filecheck" v-slot="{ field, errors }">
+            <b-form-group id="file" label="첨부파일" label-for="file">
+              <input type="file" id="formFile" @change="fileForm.changeFile" />
+              <input type="hidden" v-bind="field" />
+
+              <span class="txt-error" v-if="errors[0]">{{ errors[0] }}</span>
+            </b-form-group>
+          </Field>
+        </b-col>
+      </b-row>
+
+      <div class="text-right">
+        <b-button type="submit" variant="info" @click="fileForm.onsubmit()">Submit</b-button>
+      </div>
+    </Form>
+  </div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue';
+export default {
+  setup() {
+    const addFileForm = ref(null);
+    const fileForm = reactive({
+      name: '',
+      email: '',
+      file: null,
+
+      onsubmit: async () => {
+        const { validate } = addFileForm.value;
+
+        const { valid } = await validate();
+        if (!valid) {
+          alert('필수값을 확인해주세요.');
+        }
+      },
+
+      changeFile: () => {
+        const formFile = document.getElementById('formFile');
+
+        fileForm.file = formFile.files[0];
+      },
+    });
+
+    return {
+      addFileForm,
+      fileForm,
+    };
+  },
+};
+</script>
+
+<style>
+.text-right {
+  text-align: right;
+}
+
+.error {
+  border: 1px solid red;
+}
+
+.txt-error {
+  display: block;
+  color: red;
+}
+</style>
--- src/utils/veevalidate.js
+++ src/utils/veevalidate.js
@@ -1,12 +1,34 @@
-import { configure, defineRule } from "vee-validate";
-import AllRules from "@vee-validate/rules";
-import { localize } from "@vee-validate/i18n";
-import en from "@vee-validate/i18n/dist/locale/en.json";
-import ko from "@vee-validate/i18n/dist/locale/ko.json";
+import { configure, defineRule } from 'vee-validate';
+import AllRules from '@vee-validate/rules';
+import { localize } from '@vee-validate/i18n';
+import en from '@vee-validate/i18n/dist/locale/en.json';
+import ko from '@vee-validate/i18n/dist/locale/ko.json';
 
 // Install all rules
-Object.keys(AllRules).forEach((rule) => {
+Object.keys(AllRules).forEach(rule => {
   defineRule(rule, AllRules[rule]);
+});
+
+/* custom rules */
+/**
+ *
+ * @description : 파일 사이즈 체크
+ */
+defineRule('filecheck', (value, _, { field }) => {
+  if (!value) {
+    return `${field}항목은 필수항목입니다.`;
+  } else if (typeof value === 'object') {
+    //MB 기준으로 계산한다. 1MB = 1024 ** 2
+    const oneMb = 1024 ** 2;
+    const limitMb = 5 * oneMb; //5MB
+
+    const { size } = value;
+
+    if (size > limitMb) {
+      return `${field}항목은 5MB이하만 가능합니다.`;
+    }
+  }
+  return true;
 });
 
 configure({
