6417556463d6a6a1167325cbab1f97930bd59deb
CHOI DAE GEON <chleorjs37@gmail.com>
2022-08-20 12:32:38 +0900

    사이드바

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="sidebar">
+    <a class="active">Home</a>
+    <a>News</a>
+    <a>Contact</a>
+    <a>About</a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+  background-color: #f1f1f1;
+  height: 100%;
+  padding-right: 0;
+
+  a {
+    display: block;
+    color: black;
+    padding: 16px;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  a.active {
+    background-color: #04aa6d;
+    color: white;
+  }
+}
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,35 +1,24 @@
 <template>
   <b-row class="h-100">
-    <b-col lg="2" class="sidebar">
-      <a class="active" href="none">Home</a>
-      <a href="none">News</a>
-      <a href="none">Contact</a>
-      <a href="none">About</a>
+    <b-col lg="2" class="pr-0">
+      <Sidebar></Sidebar>
     </b-col>
-    <b-col lg="10">
+    <b-col lg="10" class="pl-0">
       <router-view />
     </b-col>
   </b-row>
 </template>
 
+<script>
+import Sidebar from '@/components/Sidebar.vue';
+export default {
+  components: {
+    Sidebar,
+  },
+};
+</script>
+
 <style lang="scss">
 @import '@/assets/styles/_variables.scss';
-
-.sidebar {
-  background-color: #f1f1f1;
-  height: 100%;
-  padding-right: 0;
-
-  a {
-    display: block;
-    color: black;
-    padding: 16px;
-    text-decoration: none;
-  }
-
-  a.active {
-    background-color: #04aa6d;
-    color: white;
-  }
-}
+@import '@/assets/styles/_styles.scss';
 </style>
