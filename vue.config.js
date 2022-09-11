e5eba5e9327fdde8d75a4e6fd89974af04e63f56
CHOI DAE GEON <chleorjs37@gmail.com>
2022-09-11 10:47:04 +0900

    캐싱 처리

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
+const path = require('path');
 module.exports = {
+  configureWebpack: config => {
+    config.output.filename = 'js/[name].[contenthash:8].min.js';
+    config.output.chunkFilename = 'js/[name].[contenthash:8].min.js';
+  },
   pluginOptions: {
     autoRouting: {
-      chunkNamePrefix: 'page-'
-    }
-  }
-}
+      chunkNamePrefix: 'page-',
+    },
+  },
+};
