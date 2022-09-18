4875bde8f09cc5fcca09b77d0a80005894a93d09
maxgunchoi <maxgunchoi@carrotglobal.com>
2022-09-19 08:45:44 +0900

    로그인 추가

--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,34 +1,79 @@
 <template>
   <div class="login">
-    <div class="login-form">
-      <h1>로그인</h1>
-      <div>
-        <b-form-input placeholder="Enter your ID"></b-form-input>
-      </div>
+    <Form @submit="loginModel.onSubmit" ref="loginForm">
+      <div class="login-form">
+        <h1>로그인</h1>
+        <div>
+          <Field v-model="loginModel.userId" id="userId" name="userId" label="아이디" rules="required|email" v-slot="{ field, errors }">
+            <b-form-input placeholder="Enter your ID" v-bind="field" :state="errors[0] ? false : null"></b-form-input>
+            <b-form-invalid-feedback v-if="errors[0]" id="input-live-feedback"> {{ errors[0] }} </b-form-invalid-feedback>
+          </Field>
+        </div>
 
-      <div class="mt-3">
-        <b-form-input placeholder="Enter your Password" type="password"></b-form-input>
-      </div>
+        <div class="mt-3">
+          <Field v-model="loginModel.password" id="password" name="password" label="Password" rules="required" v-slot="{ field, errors }">
+            <b-form-input
+              v-bind="field"
+              placeholder="Enter your Password"
+              type="password"
+              :state="errors[0] ? false : null"
+              @keyup.enter="loginModel.onSubmit()"></b-form-input>
+            <b-form-invalid-feedback v-if="errors[0]" id="input-live-feedback"> {{ errors[0] }} </b-form-invalid-feedback>
+          </Field>
+        </div>
 
-      <div class="mt-3 login-btn">
-        <b-button pill variant="primary">로그인</b-button>
+        <div class="mt-3 login-btn">
+          <b-button pill variant="primary" @click="loginModel.onSubmit()">로그인</b-button>
+        </div>
       </div>
-    </div>
+    </Form>
   </div>
 </template>
 
 <script>
 import { login } from '@/api/login';
-import { onMounted } from '@vue/runtime-core';
+import { onMounted, reactive, ref } from '@vue/runtime-core';
 export default {
   layout: 'login',
   setup() {
-    onMounted(() => {
-      login({
-        UserId: 'chleorjs37@gmail.com',
-        PassWord: 'chleorjs12@',
-      });
+    const loginForm = ref(null);
+    const loginModel = reactive({
+      userId: '',
+      password: '',
+
+      onSubmit: async () => {
+        const { validate } = loginForm.value;
+
+        const { valid } = await validate();
+
+        if (!valid) {
+          alert('필수값을 확인해주세요.');
+        } else {
+          login({
+            UserId: loginModel.userId,
+            PassWord: loginModel.password,
+          })
+            .then(res => {
+              console.log(res);
+              const { msg, success } = res.data;
+              console.log('success : ', success);
+              if (success === 'fail') {
+                alert(msg);
+              }
+            })
+            .catch(err => {
+              console.error('login  error : ', err);
+            });
+        }
+      },
     });
+
+    onMounted(() => {});
+
+    return {
+      loginForm,
+      loginModel,
+    };
   },
 };
 </script>
