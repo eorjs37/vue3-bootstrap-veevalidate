541f98266db7359c18d00888fab5385cf706c263
macdaegeon <chleorjs37@gmail.com>
2023-01-07 15:45:33 +0900

    sidetab 완료

--- src/pages/classManage.vue
+++ src/pages/classManage.vue
@@ -16,7 +16,10 @@
         <!-- sidebar -->
         <b-col cols="2">
           <div class="d-flex flex-column">
-            <div class="d-inline-block class-manage-tab-menu mb-2 p-2">전화/화상외국어과정</div>
+            <div class="d-inline-block class-manage-tab-menu mb-2 p-2">
+              전화/화상 외국어과정
+              <!-- <b-form-input class="search-input" placeholder="메뉴명을입력하세요"></b-form-input> -->
+            </div>
             <div class="d-inline-block class-manage-tab-menu mb-2 p-2">사내어학과정</div>
             <div class="d-inline-block class-manage-tab-menu mb-2 p-2">마이크로러닝과정</div>
             <div class="d-inline-block class-manage-tab-menu p-2">AI튜터과정</div>
@@ -24,8 +27,91 @@
         </b-col>
         <!-- sidebar contents -->
         <b-col cols="10">
-          <div class="class-manage-contents p-3">
-            <div class="d-flex"></div>
+          <div class="class-manage-contents p-3 overflow-auto">
+            <div class="d-flex flex-column">
+              <h5>전화/화상 외국어과정 - CTM</h5>
+              <div class="class-manage-card p-3">
+                <div class="d-flex justify-content-between">
+                  <div class="class-title mt-2">
+                    <input type="checkbox" class="cursor" />
+                    <h5 class="d-inline-block ms-3">과정명</h5>
+                  </div>
+                  <div class="notice-button">
+                    <b-button variant="primary">과정 notice</b-button>
+                    <b-button variant="primary" class="ms-2">완료 notice</b-button>
+                  </div>
+                </div>
+
+                <div class="mt-4">
+                  <table class="info">
+                    <tbody>
+                      <tr>
+                        <td class="w-20per txt-center label p-2">Company명</td>
+                        <td class="w-75per">원익IPS</td>
+                      </tr>
+                      <tr>
+                        <td class="txt-center label p-2">ID (e-Mail)</td>
+                        <td>Seoul</td>
+                      </tr>
+                      <tr>
+                        <td class="txt-center label p-2">학습자명</td>
+                        <td>Seoul</td>
+                      </tr>
+                      <tr>
+                        <td class="txt-center label p-2">연락처</td>
+                        <td>01099784424</td>
+                      </tr>
+                      <tr>
+                        <td class="txt-center label p-2">SMS/Mail 수신여부</td>
+                        <td>SMS수신</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+            <div class="d-flex flex-column mt-4">
+              <h5>전화/화상 외국어과정 - CTM</h5>
+              <div class="class-manage-card p-3">
+                <div class="d-flex justify-content-between">
+                  <div class="class-title mt-2">
+                    <input type="checkbox" class="cursor" />
+                    <h5 class="d-inline-block ms-3">과정명</h5>
+                  </div>
+                  <div class="notice-button">
+                    <b-button variant="primary">과정 notice</b-button>
+                    <b-button variant="primary" class="ms-2">완료 notice</b-button>
+                  </div>
+                </div>
+
+                <div class="mt-4">
+                  <table class="info">
+                    <tbody>
+                      <tr>
+                        <td class="w-20per txt-center label p-2">Company명</td>
+                        <td class="w-75per">원익IPS</td>
+                      </tr>
+                      <tr>
+                        <td class="txt-center label p-2">ID (e-Mail)</td>
+                        <td>Seoul</td>
+                      </tr>
+                      <tr>
+                        <td class="txt-center label p-2">학습자명</td>
+                        <td>Seoul</td>
+                      </tr>
+                      <tr>
+                        <td class="txt-center label p-2">연락처</td>
+                        <td>01099784424</td>
+                      </tr>
+                      <tr>
+                        <td class="txt-center label p-2">SMS/Mail 수신여부</td>
+                        <td>SMS수신</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
           </div>
         </b-col>
       </b-row>
