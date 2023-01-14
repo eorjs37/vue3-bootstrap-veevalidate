a65255e43440ddb8dfe7fc974d1438b26ea5b6e0
macdaegeon <chleorjs37@gmail.com>
2023-01-14 12:38:49 +0900

    vue select box 추가

--- src/pages/vueSelectBox.vue
+++ src/pages/vueSelectBox.vue
@@ -15,7 +15,14 @@
             <b-row>
               <b-col cols="2" class="label">이름 </b-col>
               <b-col cols="10">
-                <multiselect v-model="selectValue" :options="options" placeholder="Pick some" label="name" track-by="name">
+                <multiselect
+                  v-model="selectValue"
+                  :options="options"
+                  placeholder="Pick some"
+                  label="name"
+                  track-by="name"
+                  :loading="isLoading"
+                  @search-change="mockApi">
                   <template #clear>
                     <span class="vue-select-delete" v-if="selectValue" @mousedown.prevent.stop="valueClear">삭제</span>
                   </template>
@@ -31,26 +38,41 @@
 
 <script>
 import { reactive, ref } from '@vue/reactivity';
+import { onMounted } from '@vue/runtime-core';
+import mock2 from '@/assets/mock/mock2.json';
 export default {
   setup() {
     const selectValue = ref(null);
-    const options = reactive([
-      { name: 'Vue.js', language: 'JavaScript' },
-      { name: 'Adonis', language: 'JavaScript' },
-      { name: 'Rails', language: 'Ruby' },
-      { name: 'Sinatra', language: 'Ruby' },
-      { name: 'Laravel', language: 'PHP' },
-      { name: 'Phoenix', language: 'Elixir' },
-    ]);
+    const isLoading = ref(false);
+    let options = reactive([]);
 
     const valueClear = () => {
       selectValue.value = null;
     };
 
+    const mockApi = query => {
+      isLoading.value = true;
+      new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(mock2);
+        }, 2000);
+      }).then(result => {
+        options.length = 0;
+        isLoading.value = false;
+        result.forEach(item => {
+          options.push(item);
+        });
+      });
+    };
+
+    onMounted(() => {});
+
     return {
       selectValue,
+      isLoading,
       options,
       valueClear,
+      mockApi,
     };
   },
 };
