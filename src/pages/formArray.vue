8932e59f9d1b0ba55017d1cc7a5c91d171d7f88d
macdaegeon <chleorjs37@gmail.com>
2023-01-15 13:16:20 +0900

    form array 완료

--- src/pages/formArray.vue
+++ src/pages/formArray.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="container mt-3">
     <div class="class-manage-list p-3">
-      <Form @submit="onSubmit" ref="formArrary" class="form" :initial-values="initalValues">
+      <Form v-slot="{ values }" @submit.prevent="onSubmit(values)" ref="formArrary" class="form" :initial-values="initalValues">
+        <div class="mt-4 mb-4">
+          <table class="info">
+            <tbody>
+              <tr>
+                <td class="w-20per txt-center label p-2">메뉴명 <span class="required">*</span></td>
+                <td class="w-75per p-3">
+                  <Field id="companyname" name="companyname" label="회사명" rules="required" v-slot="{ field, errors }">
+                    <b-form-input id="name" v-bind="field" type="text" placeholder="회사명을 입력하세요" :state="errors[0] ? false : null">
+                    </b-form-input>
+
+                    <b-form-invalid-feedback v-if="errors[0]" id="input-live-feedback"> {{ errors[0] }} </b-form-invalid-feedback>
+                  </Field>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
         <FieldArray name="links" v-slot="{ fields }">
           <div class="d-flex flex-column mt-3" v-for="(entry, idx) in fields" :key="entry.key">
             <h5>전화/화상 외국어과정 - CTM - {{ idx + 1 }}</h5>
@@ -19,7 +36,14 @@
                     <tr>
                       <td class="w-20per txt-center label p-2">과정명 <span class="required">*</span></td>
                       <td class="w-75per p-3">
-                        <input class="form-control w-50per" type="text" placeholder="※ 사이트에 표시될 과정명을 입력해주세요" />
+                        <div class="d-inline-block w-50per">
+                          <Field id="sitename" :name="`links[${idx}].sitename`" label="과정명" rules="required" v-slot="{ field, errors }">
+                            <b-form-input id="name" v-bind="field" type="text" placeholder="과정명을 입력하세요" :state="errors[0] ? false : null">
+                            </b-form-input>
+
+                            <b-form-invalid-feedback v-if="errors[0]" id="input-live-feedback"> {{ errors[0] }} </b-form-invalid-feedback>
+                          </Field>
+                        </div>
                       </td>
                     </tr>
                     <tr>
@@ -50,7 +74,7 @@
           </div>
 
           <div class="text-right mt-3">
-            <b-button type="submit" variant="primary" @click="onSubmit">Submit</b-button>
+            <b-button type="submit" variant="primary" @click="onSubmit(values)">Submit</b-button>
           </div>
         </FieldArray>
       </Form>
@@ -63,20 +87,24 @@ import { reactive } from '@vue/reactivity';
 export default {
   setup() {
     const initalValues = {
+      companyname: '',
       links: [
         {
           idx: 1,
           text: 1,
+          sitename: '',
           standard: 'Y',
         },
         {
           idx: 1,
           text: 1,
+          sitename: '',
           standard: 'N',
         },
         {
           idx: 1,
           text: 1,
+          sitename: '',
           standard: 'N',
         },
       ],
