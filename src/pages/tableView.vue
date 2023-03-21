3d9094acc4778a6005ce7433d675463d8bc3c347
macdaegeon <chleorjs37@gmail.com>
2023-03-21 12:53:46 +0900

    table view 설명 준비

---
+++ src/pages/tableView.vue
@@ -0,0 +1,62 @@
+<template>
+  <h1>table view</h1>
+  <Table :tabledata="tableData" :isCheckBox="false" @rowselected="onSelectRow">
+    <!--##### Head #####-->
+    <template #Headid> 커스텀ID </template>
+    <!--##### Body #####-->
+    <template #Bodyid="{ row }"> {{ row.id }}번 </template>
+  </Table>
+</template>
+
+<script>
+import { reactive } from 'vue';
+export default {
+  setup() {
+    const tableData = reactive({
+      head: [
+        {
+          headkey: 'id',
+          colname: 'ID',
+        },
+        {
+          headkey: 'Company',
+          colname: '회사명',
+        },
+        {
+          headkey: 'Contury',
+          colname: '국가',
+        },
+        {
+          headkey: 'Name',
+          colname: '이름',
+        },
+      ],
+      body: [
+        {
+          id: 1,
+          Company: 'Alfreds Futterkiste',
+          Name: 'Maria Anders',
+          Contury: 'Germany',
+        },
+        {
+          id: 2,
+          Company: 'Alfreds Futterkiste',
+          Name: 'Maria Anders',
+          Contury: 'Germany',
+        },
+      ],
+    });
+
+    const onSelectRow = item => {
+      //emit을 통해서 선택된 row return
+      console.log(item);
+    };
+    return {
+      tableData,
+      onSelectRow,
+    };
+  },
+};
+</script>
+
+<style></style>
